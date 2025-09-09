d6489a26792062afcc19ea5d08d53c409e902820
Alexander Christensen <alexchristensen553@gmail.com>
2025-09-09 22:33:52 +0200

    Change Comment

--- Øve Opgaver/1_Datatyper.py
+++ Øve Opgaver/1_Datatyper.py
@@ -3,7 +3,7 @@
 # Eksempel 1: Helt simple typer
 a_int = 42
 a_float = 3.14
-a_str = "hej"
+a_str = "hello"
 a_bool = True
 print(type(a_int), type(a_float), type(a_str), type(a_bool))
 
@@ -11,18 +11,18 @@ print(type(a_int), type(a_float), type(a_str), type(a_bool))
 a_list = [1, 2, 3]
 a_tuple = (4, 5, 6)
 a_set = {1, 1, 2, 3}     # et set fjerner dubletter -> {1,2,3}
-a_dict = {"navn": "Alex", "alder": 23}
+a_dict = {"navn": "henrik", "alder": 39}
 print(a_list, a_tuple, a_set, a_dict)
 
 # Eksempel 3: Lister kan ændres, tuples kan ikke
 a_list.append(4)
 print("Liste efter append:", a_list)
 
-# Eksempel 4: None er "ingenting"
+# Eksempel 4: None er "ingenting" når jeg siger det er således
 nothing = None
 print("Er nothing None?", nothing is None)
 
-# Eksempel 5: Tjekke om et objekt har længde
+# Eksempel 5: Tjekke om et objekt har længde længere end lort
 def safe_len(obj):
     return len(obj) if hasattr(obj, "__len__") else 0
 print("safe_len på liste:", safe_len([1,2,3]), "| safe_len på 10:", safe_len(10))
