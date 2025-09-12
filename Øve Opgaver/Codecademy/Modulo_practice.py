7a1fcdbee52d7afe0203a32917638b7e944624c1
Alexander Christensen <alexchristensen553@gmail.com>
2025-09-12 15:39:04 +0200

    Modul + String_concatenation tasks done

---
+++ Øve Opgaver/Codecademy/Modulo_practice.py
@@ -0,0 +1,8 @@
+first_order_remainder = 269 % 10
+second_order_remainder = 270 % 10
+
+first_order_coupon = "no"
+second_order_coupon = "yes"
+
+print(first_order_remainder)
+print(second_order_remainder)
